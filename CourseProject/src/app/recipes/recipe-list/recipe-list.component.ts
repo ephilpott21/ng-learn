ed7ac36b16635c9f5adf6341d7c115a89a3c534a
ephilpott <eli.philpott21@gmail.com>
2022-11-21 13:58:43 -0700

    refactored recipes and added navigation to header bar

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../shared/recipe.model';
 
 @Component({
@@ -7,7 +7,8 @@ import { Recipe } from '../../shared/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  @Output() recipes: Recipe[] = [
     new Recipe('Biscuits & Gravy',
         'Biscuits with Sausage Gravy',
         'https://tinyurl.com/3wvhr8s4'),
@@ -20,5 +21,10 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+   }
 
 }
